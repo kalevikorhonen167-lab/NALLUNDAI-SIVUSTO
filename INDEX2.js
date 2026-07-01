e75b4b215fc275a5c0d19f559827b379325e887d
kalevikorhonen167-lab <kalevikorhonen167@gmail.com>
2026-07-01 20:16:50 +0300

    Update INDEX2.js

--- INDEX2.js
+++ INDEX2.js
@@ -470,6 +470,7 @@ async function deleteNotification(index) {
     
     // Päivitetään näkymä välittömästi
     showNotifications();
+}
 // ---------------- DIGIKOLIKKO-GRAAFI ----------------
 async function initChart() {
     const canvas = document.getElementById('digikolikkoChart');
@@ -477,7 +478,7 @@ async function initChart() {
 
     // Haetaan yhteinen historia tietokannasta
     const histSnap = await getDoc(doc(db, "digikolikko", "hintaHistoria"));
-    let history = histSnap.exists() ? histSnap.data().list : [{time: "Alku", price: 15000}];
+    let history = histSnap.exists() ? histSnap.data().list : [{time: "Alku", price: 20000}];
 
     // Luodaan graafi
     digikolikkoChart = new Chart(canvas.getContext('2d'), {
@@ -490,13 +491,22 @@ async function initChart() {
                 borderColor: '#22c55e',
                 backgroundColor: 'rgba(34, 197, 94, 0.2)',
                 fill: true,
-                tension: 0.3
+                tension: 0.1
             }]
         },
         options: { 
             responsive: true, 
             maintainAspectRatio: false,
-            scales: { y: { beginAtZero: false } }
+            scales: { 
+                y: { 
+                    beginAtZero: false,
+                    min: 9000,   // Kiinteä alaraja
+                    max: 100000, // Kiinteä yläraja
+                    ticks: {
+
+                    }
+                } 
+            }
         }
     });
 }
@@ -508,12 +518,18 @@ async function updateChart(newPrice) {
     
     const time = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
     
+    // ESTETÄÄN TUPLAPISTEET: Tarkistetaan, onko viimeisin piste sama kuin uusi
+    if (history.length > 0) {
+        const last = history[history.length - 1];
+        if (last.price === newPrice) return; 
+    }
+    
     // Lisätään uusi piste
     history.push({ time, price: newPrice });
     
-    // Pidetään listan pituus maksimissaan 10:ssä
-    while (history.length > 200) {
-        history.shift(); // Poistaa vanhimman
+    // Pidetään listan pituus maksimissaan 25:ssä
+    while (history.length > 25) {
+        history.shift(); 
     }
     
     // Tallennetaan päivitetty lista Firestoreen
